24faf0533a36d7fc8e88d03eb54903c9d184dc29
paperbenni <paperbenni@gmail.com>
2018-07-30 10:06:25 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt update
+sudo apt install blender\
+ krita\
+ python\
+ curl\
+ vlc\
+ ubuntu-restricted-extras\
+ ffmpeg\
+ git\
+ gimp\
+ arc-theme\
+ gparted\
+ clang\
+ curl\
+ neovim\
+ openjdk-8-jdk openjdk-8-demo openjdk-8-doc openjdk-8-jre-headless openjdk-8-source
+ 
